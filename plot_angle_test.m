5cee02e1b0d2b0d61f41d47dbf95d5bc622e8d4b
Eric Miller <eric@legoaces.org>
2015-12-04 13:45:30 -0500

    Initial commit

---
+++ plot_angle_test.m
@@ -0,0 +1,8 @@
+Angles = [0, 15, 20, 25, 30, 35, 40, 45, 50, 60, 70, 80, 90];
+Measurements = [2.498, 2.48, 2.2, 1.8, 1.5, 0.92, 0.58, 0.53, 0.36, 0.278, 0.13, 0.068, 0.058];
+
+clf
+hold on
+plot(Angles(2:end), Measurements(2:end));
+plot(linspace(0,90), exp(1.85)*exp(-0.054*linspace(0,90)));
+xlim([0 90])
---
+++ test_analog_discovery.m
@@ -0,0 +1,30 @@
+
+% SEE: http://www.mathworks.com/help/daq/examples/getting-started-acquiring-data-with-digilent-analog-discovery.html
+
+s = daq.createSession('digilent')
+
+ch = addAnalogInputChannel(s,'AD1', 1, 'Voltage')
+ch2 = addAnalogInputChannel(s,'AD1', 2, 'Voltage')
+
+pause
+
+[singleReading, triggerTime] = s.inputSingleScan
+
+
+while 1
+    pause
+
+    s.Rate = 300e3;
+    s.Channels(1).Range = [-2.5 2.5];
+    s.Channels(2).Range = [-2.5 2.5];
+    s.DurationInSeconds = 0.1
+
+    [data, timestamps, triggerTime] = s.startForeground;
+
+    plot(timestamps, data);
+    xlabel('Time (seconds)')
+    ylabel('Voltage (Volts)')
+    title(['Clocked Data Triggered on: ' datestr(triggerTime)])
+    legend('1', '2')
+    
+end
