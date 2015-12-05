f817fabda98940c09a8f96ab96292868e1555e42
Eric Miller <eric@legoaces.org>
2015-12-05 15:03:07 -0500

    Added comments

--- plot_angle_test.m
+++ plot_angle_test.m
@@ -3,6 +3,6 @@ Measurements = [2.498, 2.48, 2.2, 1.8, 1.5, 0.92, 0.58, 0.53, 0.36, 0.278, 0.13,
 
 clf
 hold on
-plot(Angles(2:end), Measurements(2:end));
+plot(Angles(2:end), Measurements(2:end)); %exclude maxed-out first measurement
 plot(linspace(0,90), exp(1.85)*exp(-0.054*linspace(0,90)));
 xlim([0 90])
